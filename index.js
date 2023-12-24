a6328d39509a7e06fe3400969234f1a60ab9802e
pixShopping <pixshopping021@gmail.com>
2023-12-24 12:16:37 -0300

    atualizacao 2

--- index.js
+++ index.js
@@ -14,70 +14,122 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 async function consultarDados(nome) {
     
-    const browser = await puppeteer.launch({
-        headless: true, // Torna o navegador visível usar FALSE
-    });
+  const browser = await puppeteer.launch({
+      headless: true, // Torna o navegador visível usar FALSE
+  });
 
-    const page = await browser.newPage();
+  const page = await browser.newPage();
 
-    await page.goto(`https://webmii.com/people?n="${nome}"#gsc.tab=0&gsc.q="${nome}"&gsc.sort=date`)
+  await page.goto(`https://webmii.com/people?n="${nome}"#gsc.tab=0&gsc.q="${nome}"&gsc.sort=date`)
 
-    await page.waitForSelector('#news');
-    await page.waitForSelector('#web');
+  await page.waitForSelector('#news');
+  await page.waitForSelector('#web');
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));  
+  await new Promise((resolve) => setTimeout(resolve, 5000));  
 
-    // Extrair informações do NEWS
-    const resultsNews = await page.evaluate(() => {
-        const newsDiv = document.querySelector('#news');
-        const resultDivs = newsDiv.querySelectorAll('.resultdate-box');
+  // Extrair informações do NEWS
+  const resultsNews = await page.evaluate(() => {
+      const newsDiv = document.querySelector('#news');
+      const resultDivs = newsDiv.querySelectorAll('.resultdate-box');
 
-        const dataNews = [];
+      const dataNews = [];
 
-        resultDivs.forEach((resultDiv) => {
-            const titulo = resultDiv.querySelector('.resultdate-title').innerText;
-            const texto = resultDiv.querySelector('.resultdate-snippet').innerText;
-            const dominio = resultDiv.querySelector('.resultdate-domainname').innerText;
+      resultDivs.forEach((resultDiv) => {
+          const titulo = resultDiv.querySelector('.resultdate-title').innerText;
+          const texto = resultDiv.querySelector('.resultdate-snippet').innerText;
+          const dominio = resultDiv.querySelector('.resultdate-domainname').innerText;
 
-            dataNews.push({
-                titulo,
-                texto,
-                dominio,
-            });
-        });
+          dataNews.push({
+              titulo,
+              texto,
+              dominio,
+          });
+      });
 
-        return dataNews;
-    });
+      return dataNews;
+  });
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    const resultsWeb = await page.evaluate(() => {
-        const newsDiv = document.querySelector('#web');
-        const resultDivs = newsDiv.querySelectorAll('.result-box');
+  const resultsWeb = await page.evaluate(() => {
+      const newsDiv = document.querySelector('#web');
+      const resultDivs = newsDiv.querySelectorAll('.result-box');
 
-        const dataWeb = [];
+      const dataWeb = [];
 
-        resultDivs.forEach((resultDiv) => {
-            const titulo = resultDiv.querySelector('.result-title').innerText;
-            const texto = resultDiv.querySelector('.result-snippet').innerText;
-            const dominio = resultDiv.querySelector('.result-domainname').innerText;
+      resultDivs.forEach((resultDiv) => {
+          const titulo = resultDiv.querySelector('.result-title').innerText;
+          const texto = resultDiv.querySelector('.result-snippet').innerText;
+          const dominio = resultDiv.querySelector('.result-domainname').innerText;
 
-            dataWeb.push({
-                titulo,
-                texto,
-                dominio,
-            });
-        });
+          dataWeb.push({
+              titulo,
+              texto,
+              dominio,
+          });
+      });
 
-        return dataWeb;
-    });
+      return dataWeb;
+  });
 
-    await browser.close();
+  await browser.close();
 
-    return {
-      websites: resultsWeb,
-      noticias: resultsNews,
-    };
+  return {
+    websites: resultsWeb,
+    noticias: resultsNews,
+  };
+
+}
+
+// CONSULTAR CPF
+async function consultarCpf(nome, estado, cidade) {
+  const browser = await puppeteer.launch({
+      headless: true, // Torna o navegador visível usar FALSE
+  });
+
+  const page = await browser.newPage();
+  //await page.browserContext().overridePermissions('https://buscaprime.com.br', ['geolocation']);
+
+  await page.goto(`https://buscaprime.com.br/buscar-dados-pelo-nome/consulta.php`);
+
+  await new Promise((resolve) => setTimeout(resolve, 2500));
+
+  // ABRIR FORMULARIO INTEIRO
+  await page.evaluate(() => {
+    // Encontre o elemento pelo ID e adicione a classe 'show'
+    const element = document.getElementById('collapseOne7');
+    if (element) {
+      element.classList.add('show');
+    }
+  });
+  
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  await page.waitForSelector('input[name="searchTerms[name]"]');
+  await page.type('input[name="searchTerms[name]"]', nome);
+
+  await page.waitForSelector('select[name="searchTerms[state]"]');
+  await page.select('select[name="searchTerms[state]"]', estado);
+
+  await page.waitForSelector('input[name="searchTerms[city]"]');
+  await page.type('input[name="searchTerms[city]"]', cidade);
+  
+  await page.evaluate(() => {
+    const botao = document.querySelector('button[type="submit"]'); // Substitua pelo ID correto do botão
+    if (botao) {
+      botao.click();
+    }
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  
+  return {
+    nome: nome,
+    estado: estado,
+    cidade: cidade,
+  };
+
+  //await browser.close();
 
 }
 
@@ -89,7 +141,7 @@ app.get('/consultar', (req, res) => {
   
   // Verifica se o usuário e a senha foram fornecidos na URL
   if (!nome) {
-    return res.status(400).json({ error: 'Usuário, Senha são obrigatórios.' });
+    return res.status(400).json({ error: 'Nome é obrigatório.' });
   }
 
   // Chama a função para preencher o formulário e fazer login com os parâmetros recebidos
@@ -102,6 +154,28 @@ app.get('/consultar', (req, res) => {
       return res.status(500).json({ error: 'Erro interno do servidor' });
     });
 });
+
+app.get('/cpf', (req, res) => {
+  const nome = req.query.nome;
+  const estado = req.query.estado;
+  const cidade = req.query.cidade;
+  
+  // Verifica se o usuário e a senha foram fornecidos na URL
+  if (!nome || !estado || !cidade) {
+    return res.status(400).json({ error: 'Nome, Estado e Cidade são obrigatórios.' });
+  }
+
+  // Chama a função para preencher o formulário e fazer login com os parâmetros recebidos
+  consultarCpf(nome, estado, cidade)
+    .then((data) => {
+      return res.json(data);
+    })
+    .catch((error) => {
+    console.error('Erro ao consultar dados', error);
+      return res.status(500).json({ error: 'Erro interno do servidor' });
+    });
+});
+
 
 // Inicializa o servidor
 app.listen(port, () => {
